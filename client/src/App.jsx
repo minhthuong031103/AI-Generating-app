86278b6c7f8158ad9b1be8ced8a015f28656ebb4
minhthuong031103 <nominh67@gmail.com>
2023-05-09 13:04:57 +0700

    Build

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+
+const App = function () {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+export default App;
