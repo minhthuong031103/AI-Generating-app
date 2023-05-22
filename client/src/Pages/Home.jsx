52cd7c1e3adb923a633fd0301d23396445a0dc83
minhthuong031103 <nominh67@gmail.com>
2023-05-22 22:25:27 +0700

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
     setLoading(true);
     try {
       const response = await fetch(
-        'https://aigeneratingapp.onrender.com//api/v1/post/all',
+        'aigeneratingapp.onrender.com//api/v1/post/all',
         {
           method: 'GET',
           headers: {
