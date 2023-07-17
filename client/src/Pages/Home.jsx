0346121e4294844957c388b34c8d6dd37074862b
minhthuong031103 <nominh67@gmail.com>
2023-07-17 22:04:27 +0700

    Update Home.jsx

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
     setLoading(true);
     try {
       const response = await fetch(
-        'https://aigeneratingapp.onrender.com/api/v1/post/all',
+        'https://ai-care.onrender.com/api/v1/post/allofuser',
         {
           method: 'GET',
           headers: {
